a37af89deb8e63f643c967a97757dd2654ddae70
pedrozopayares <pedrozopayares@gmail.com>
2020-09-30 22:43:03 -0500

    Update to message
    You still don't get the biggest XD

--- theBiggest.py
+++ theBiggest.py
@@ -1,11 +1,12 @@
+# The Biggest is ...
 num = input("Enter a number with several digits: ")
 digits = str(num)
 digitsLenght = len(digits)
 biggest = 0
 for i in range(digitsLenght):
-     for j in range (i - 1):
+     for j in range (digitsLenght-1):
           if digits[j] > digits[j+1]:
                 biggest = digits[j]
           else:
                 biggest = digits[j+1]
-print(biggest)
+print("The biggest digit in number is:" + biggest)
