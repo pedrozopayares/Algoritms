07947d281f0a6578cdf783b3038fcbacc145eb56
pedrozopayares <pedrozopayares@gmail.com>
2020-09-30 22:11:13 -0500

    Obteniendo el mayor
    Obteniendo el elemento mayor en toda la cadena

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,2 +1,12 @@
-for i in range(5):
-...     print(i)
+numero = input("Ingrese un número: ")
+numeroString = str(numero)
+numeroLenght = len(numeroString)
+mayor = 0
+for i in range(numeroLenght):
+     espacioDesordenado = i
+     for j in range (espacioDesordenado - 1):
+          if numeroString[j] > numeroString[j+1]:
+                mayor = numeroSstring[j]
+          else:
+                mayor = numeroString[j+1]
+print(mayor)
